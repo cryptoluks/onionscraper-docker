643b4080e288c198f7d4ae17ede1bca15950cbbe
cryptoluks <9020527+cryptoluks@users.noreply.github.com>
2024-07-11 21:36:45 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+mkdir -p cache
 docker run \
   -v $(pwd)/roms:/roms \
+  -v $(pwd)/cache:/root/.skyscraper/cache \
   -v $(pwd)/config.ini:/root/.skyscraper/config.ini \
   -it ghcr.io/cryptoluks/onionscraper-docker \
   onionscraper -a
